786e71a526cfff30bf052aaa1053bf5c59b7d3d1
Bahaa Jr <geekbahaa1@gmail.com>
2018-12-16 02:10:05 +0200

    Changing the message context and removed some unncessary code

--- background.js
+++ background.js
@@ -17,24 +17,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   const { action } = request
 
 });
-
-const callback = function(details) {
-  console.log("callback");
-  if (details.url.includes("memberConnections")) {
-   console.log(details);
-  }
-
-};
-const filter = {
-  urls: ["<all_urls>"],
-  types: ["xmlhttprequest"],
-};
-const opt_extraInfoSpec = [];
-
-const responseListener = (details) => {
-  console.log(details);
-}
-
-chrome.webRequest.onHeadersReceived.addListener(responseListener, filter,["blocking", "responseHeaders"]);
-
-// chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ window.onload = () => {
   const connections = contains("code", "com.linkedin.voyager.identity.shared.MemberConnection")[0]
     .innerText.match(/"paging":\{"total":\d+/gi)[0].match(/\d+/gi)[0]
   console.log(connections);
-  document.querySelector(".pv-top-card-v2-section__connections").innerText = `${commas(connections)} Connections`
+  document.querySelector(".pv-top-card-v2-section__connections").innerText = `See connections (${commas(connections)})`
 }
